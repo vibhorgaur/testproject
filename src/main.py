63fd9156b44205177d5ade85379a83895a958171
Vibhor Gaur <vibhorgaur@Vibhors-MacBook-Pro.local>
2024-12-16 11:22:18 -0800

    changes to main.py incorporating error handling

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
 import requests
+import validators
 
-respose = requests.get("https://api.github.com")
-print(respose.json())
+url = "https://api.github.com"
+
+if validators.url(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status() # raise an HTTPError for bad responses (4xx and 5xx)
+        print(response.json())
+    except requests.exceptions.RequestException as e: 
+        print(f"An error occurred: {e}")
+else:
+    print("Invalid URL.")
